fbeb666e6658f038e3bf4b48dae43a5e3db9c235
Diego Poveda <alejopov@gmail.com>
2022-04-11 16:16:11 -0500

    Add missing http methods allowed

--- src/main/java/com/udea/empleados/config/CorsConfig.java
+++ src/main/java/com/udea/empleados/config/CorsConfig.java
@@ -28,7 +28,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedMethods("GET", "POST")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowedOrigins("http://localhost:3000");
             }
